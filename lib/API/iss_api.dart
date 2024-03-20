b36bc04f0183dd6c82dc83f35bb191366d33cb89
PepitoLeFameux <123roman35@gmail.com>
2024-03-20 18:05:03 +0100

    Map se met à jour sans rafraîchir tout le widget et affichage de la vitesse

--- lib/API/iss_api.dart
+++ lib/API/iss_api.dart
@@ -1,15 +1,29 @@
 import 'dart:async';
 import 'dart:convert';
+import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
+import 'package:geolocator/geolocator.dart';
 
 class IssApi {
+
+  //Pour faire un singleton
+  static final IssApi _instance = IssApi._privateConstructor();
+  IssApi._privateConstructor();
+  static IssApi get instance => _instance;
+  static double? exLat, exLon, exTime;
+
   Future<Map<String, double>> getIssPosition() async {
     final response = await http.get(Uri.parse('http://api.open-notify.org/iss-now.json'));
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
       final double lat = double.parse(data['iss_position']['latitude']);
       final double lon = double.parse(data['iss_position']['longitude']);
-      return {'latitude': lat, 'longitude': lon};
+      final double time = data['timestamp'].toDouble();
+      final double speed = (exLon != null) ? Geolocator.distanceBetween(exLat!, exLon!, lat, lon)/(time - exTime!): 0;
+      exLon = lon;
+      exLat = lat;
+      exTime = time;
+      return {'latitude': lat, 'longitude': lon, 'speed': speed};
     } else {
       throw Exception('Failed to load ISS position');
     }
--- lib/IssMap.dart
+++ lib/IssMap.dart
@@ -1,41 +1,150 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'API/iss_api.dart';
 
 class IssMap extends StatelessWidget {
-  final LatLng issPosition;
-
-  IssMap({required this.issPosition});
 
   @override
   Widget build(BuildContext context) {
-    return FlutterMap(
-      options: MapOptions(
-        center: issPosition,
-        zoom: 2.0,
-      ),
+    return Stack(
       children: [
-        TileLayer(
+        FlutterMap(
+          options: MapOptions(
+            zoom: 2.0,
+          ),
+          children: [
+            TileLayer(
 
-            urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-            subdomains: ['a', 'b', 'c'],
+                urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                subdomains: ['a', 'b', 'c'],
 
+            ),
+            IssMarker(),
+          ],
         ),
-        MarkerLayer(
+        Align(
+          alignment: Alignment.topRight,
+          child: SpeedCard(),
+        )
+      ]
+    );
+  }
+}
 
+class IssMarker extends StatefulWidget {
+  @override
+  State<IssMarker> createState() => _IssMarkerState();
+}
+
+class _IssMarkerState extends State<IssMarker> {
+  late Future<Map<String, double>> position = IssApi.instance.getIssPosition();
+  LatLng? exPosition;
+
+  void updatePosition(){
+    setState(() {
+      position = IssApi.instance.getIssPosition();
+    });
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    Timer.periodic(Duration(seconds: 1), (Timer t) => updatePosition());
+  }
+
+  @override
+  Widget build(BuildContext buildContext){
+    return FutureBuilder<Map<String, double>>(
+      future: position,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return exPosition == null ? Center(child: CircularProgressIndicator()) :
+          MarkerLayer(
             markers: [
               Marker(
                 width: 80.0,
                 height: 80.0,
-                point: issPosition,
+                point: exPosition!,
                 builder: (ctx) => Container(
                     child: Image.asset('images/iss.png')
                 ),
               ),
             ],
+          );
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else {
+          LatLng latlng = LatLng(snapshot.data!['latitude']!, snapshot.data!['longitude']!);
+          exPosition = latlng;
+          return MarkerLayer(
+            markers: [
+              Marker(
+                width: 80.0,
+                height: 80.0,
+                point: latlng,
+                builder: (ctx) => Container(
+                    child: Image.asset('images/iss.png')
+                ),
+              ),
+            ],
+          );
+        }
+      }
+    );
+  }
+}
 
-        ),
-      ],
+class SpeedCard extends StatefulWidget{
+  @override
+  State<SpeedCard> createState() => _SpeedCardState();
+}
+
+class _SpeedCardState extends State<SpeedCard> {
+  late Future<Map<String, double>> position = IssApi.instance.getIssPosition();
+  double exSpeed = 0;
+
+  void updateSpeed() {
+    setState(() {
+      position = IssApi.instance.getIssPosition();
+    });
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    Timer.periodic(Duration(seconds: 1), (Timer t) => updateSpeed());
+  }
+
+  @override
+  Widget build(BuildContext buildContext){
+    return FutureBuilder<Map<String, double>>(
+        future: position,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return exSpeed == 0 ? Center(child: CircularProgressIndicator()) :
+            Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text("Speed : ${exSpeed.toStringAsFixed(0)} m/s"),
+                )
+            );
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else {
+            double speed = snapshot.data!['speed']!;
+            exSpeed = speed;
+            return Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text("Speed : ${speed.toStringAsFixed(0)} m/s"),
+                )
+            );
+          }
+        }
     );
   }
 }
