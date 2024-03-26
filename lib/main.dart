e4e6d8606c4eee1f424f754627a58f420d1e9e44
PepitoLeFameux <123roman35@gmail.com>
2024-03-26 11:28:19 +0100

    ISS qui glisse et remplacement de l'API pour historique

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Nasa Now',
         theme: ThemeData(
           useMaterial3: true,
